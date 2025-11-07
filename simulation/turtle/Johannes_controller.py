c5f10edfcb2c9d6d63124b55ea50f9b5df98a3e4
Johannes-Lopitz <johannes.lopitz@web.de>
2025-11-07 11:12:14 +0100

    functioning movement without backup

--- simulation/turtle/Johannes_controller.py
+++ simulation/turtle/Johannes_controller.py
@@ -24,13 +24,11 @@ class JohannesController(Node):
         self.agent_list = agent_list
         self.turtle_bots = {}  # {agent: {"name": "tb1", "drive": client, "rotate": client}}
         self.positions = {}
-        self.robot_should_rotate_back = False
-        self.robot_rotated = False
         self.start_positions = {}
-        self.turned_one_time = False
-        self.turned_second_time = False
         self.current_drive_goal = None
         self.current_rotate_goal = None
+        self.turned_back = False
+        self.turn_back = False
 
 
         # Map each agent to a TurtleBot name (tb1, tb2, tb3, tb4)
@@ -118,32 +116,36 @@ class JohannesController(Node):
         drive_client = tb["drive"]
         drive_client.wait_for_server()
         send_goal_future = drive_client.send_goal_async(goal)
+        self.get_logger().warn(f"drive forward goal set")
         send_goal_future.add_done_callback(
-            lambda f: self.move_forward_response_callback(f, agent, distance, angle)
+            lambda f: self.move_forward_response_callback(f, agent, angle)
         )
 
-    def move_forward_response_callback(self, future, agent, distance, angle):
+    def move_forward_response_callback(self, future, agent, angle):
         self.get_logger().warn(f"drive forward command in progress")
         goal_handle = future.result()
         self.current_drive_goal = future.result()
         get_result_future = goal_handle.get_result_async()
         get_result_future.add_done_callback(
-            lambda f: self.move_forward_result_callback(f, agent, distance, angle)
+            lambda f: self.move_forward_result_callback(f, agent, angle)
         )
 
-    def move_forward_result_callback(self, future, agent, distance, angle):
+    def move_forward_result_callback(self, future, agent, angle):
         result = future.result()
         if result.status == GoalStatus.STATUS_CANCELED:
-            self.get_logger().warn("Drive command was cancelled, ignoring result callback.")
+            self.get_logger().warn("Move forward command was cancelled, ignoring result callback.")
             return
         self.get_logger().warn(f"drive forward command finished")
-        if self.robot_should_rotate_back == True:
-            self.get_logger().warn(f"rotate robot command send after movement")
-            self.robot_should_rotate_back = False
-            self.rotate_robot(agent, distance, -angle)
+        if self.turn_back == True:
+            self.turn_back = False
+            self.turned_back = True
+            self.rotate_robot(agent, 0.0, -angle)
         else:
             self.environment.robot_reached_position()
-            self.robot_rotated = False
+
+
+
+
 
     def rotate_robot(self, agent, distance, angle):
         self.get_logger().warn(f"rotate command received")
@@ -171,110 +173,26 @@ class JohannesController(Node):
     def rotate_robot_result_callback(self, future, agent, distance, angle):
         result = future.result()
         if result.status == GoalStatus.STATUS_CANCELED:
-            self.get_logger().warn("Rotate command was cancelled, ignoring result callback.")
+            self.get_logger().warn("Move forward command was cancelled, ignoring result callback.")
             return
         self.get_logger().warn(f"rotate command finished")
-        self.robot_rotated = True
-        if self.robot_should_rotate_back == False:
-            self.get_logger().warn(f"drive forward command send after rotation")
-            self.move_forward(agent, distance, angle)
-        else:
+        if self.turned_back == True:
+            self.turned_back = False
             self.environment.robot_reached_position()
-            self.robot_rotated = False
-            self.robot_should_rotate_back = False
+        else:
+            self.get_logger().warn(f"drive forward command send after rotation")
+            self.turn_back = True
+            self.move_forward(agent, distance, angle)
     
     # ------------------------------------
     # Movements to return to start position
     # ------------------------------------
-
-    def rotate_robot_to_start_pos(self, agent, distance, angle1, angle2):
-        self.get_logger().warn(f"rotate to start pos command received")
-        goal = RotateAngle.Goal()
-        drive_goal = DriveDistance.Goal()
-        if self.turned_one_time == False:
-            goal.angle = angle1
-        else:
-            goal.angle = angle2
-            self.turned_second_time = True
-        self.get_logger().warn(f"rotate to start pos command received with angle: {goal.angle}")
-        self.get_logger().warn(f"self.turned_second_time: {self.turned_second_time}")
-        goal.max_rotation_speed = 1.0
-        tb = self.get_turtlebot(agent)
-        rotate_client = tb["rotate"]
-        rotate_client.wait_for_server()
-        send_goal_future = rotate_client.send_goal_async(goal)
-        send_goal_future.add_done_callback(
-            lambda f: self.rotate_robot_to_start_pos_response_callback(f, agent, distance, angle1, angle2)
-        )
-    
-    def rotate_robot_to_start_pos_response_callback(self, future, agent, distance, angle1, angle2):
-        self.get_logger().warn(f"rotate to start pos command in progress")
-        goal_handle = future.result()
-        get_result_future = goal_handle.get_result_async()
-        get_result_future.add_done_callback(
-            lambda f: self.rotate_robot_to_start_pos_result_callback(f, agent, distance, angle1, angle2)
-        )
-    
-    def rotate_robot_to_start_pos_result_callback(self, future, agent, distance, angle1, angle2):
-        self.get_logger().warn(f"rotate to start pos command finished")
-        if self.turned_second_time == False:
-            self.get_logger().warn(f"drive forward to start pos command send after rotation")
-            self.move_robot_to_start_pos(agent, distance, angle1, angle2)
-        else:
-            self.environment.robot_reached_position()
-
-    def move_robot_to_start_pos(self, agent, distance, angle1, angle2):
-        self.get_logger().warn(f"drive forward to start pos command received")
-        goal = DriveDistance.Goal()
-        goal.distance = distance
-        goal.max_translation_speed = 0.2
-        tb = self.get_turtlebot(agent)
-        drive_client = tb["drive"]
-        drive_client.wait_for_server()
-        send_goal_future = drive_client.send_goal_async(goal)
-        send_goal_future.add_done_callback(
-            lambda f: self.move_forward_to_start_pos_response_callback(f, agent, distance, angle1, angle2)
-        )
-        
-    def move_forward_to_start_pos_response_callback(self, future, agent, distance, angle1, angle2):
-        self.get_logger().warn(f"drive forward to start pos command in progress")
-        goal_handle = future.result()
-        get_result_future = goal_handle.get_result_async()
-        get_result_future.add_done_callback(
-            lambda f: self.move_forward_to_start_pos_result_callback(f, agent, distance, angle1, angle2)
-        )
-
-    def move_forward_to_start_pos_result_callback(self, future, agent, distance, angle1, angle2):
-        self.get_logger().warn(f"drive forward to start pos command finished")
-        self.get_logger().warn(f"rotate back to start orientation command send")
-        self.turned_one_time = True
-        self.rotate_robot_to_start_pos(agent, distance, angle1, angle2)
 
 
     
     # ------------------------------------
     # Return to start position command
     # ------------------------------------
-    def return_to_start_pos(self, agent):
-        tb_name = self.get_turtlebot(agent)["name"]
-        if tb_name not in self.positions or tb_name not in self.start_positions:
-            self.get_logger().warn(f"[{tb_name}] Keine Position oder Startposition verfügbar.(return_to_start_pos)")
-            return
-
-        current = self.positions[tb_name]
-        start = self.start_positions[tb_name]
-
-        cx, cy, cyaw = current["x"], current["y"], current["yaw"]
-        sx, sy, syaw = start["x"], start["y"], start["yaw"]
-
-        dx = sx - cx
-        dy = sy - cy
-        target_angle = math.atan2(dy, dx)
-        rotate1 = target_angle - cyaw
-        distance = math.sqrt(dx*dx + dy*dy)
-        rotate2 = syaw - target_angle
-        self.rotate_robot_to_start_pos(agent, distance, rotate1, rotate2)
-
 
 
     # ------------------------------------
@@ -282,36 +200,26 @@ class JohannesController(Node):
     # ------------------------------------
     def move_top(self, agent):
         self.environment.robot_is_moving()
-        self.robot_rotated = False
-        self.robot_should_rotate_back = False
-        self.save_start_pos(agent)
         self.get_logger().warn(f"drive forward command send")
         self.move_forward(agent, 0.3, 0.0)
         self.get_logger().warn(f"function move_top finished")
 
     def move_left(self, agent):
         self.environment.robot_is_moving()
-        self.robot_rotated = True
-        self.robot_should_rotate_back = False
-        self.save_start_pos(agent)
         self.get_logger().warn(f"rotate left command send")
         self.rotate_robot(agent, 0.3, 1.57)
         self.get_logger().warn(f"function move_left finished")
 
     def move_right(self, agent):
         self.environment.robot_is_moving()
-        self.robot_rotated = True
-        self.robot_should_rotate_back = False
-        self.save_start_pos(agent)
         self.get_logger().warn(f"rotate right command send")
         self.rotate_robot(agent, 0.3, -1.57)
         self.get_logger().warn(f"function move_right finished")
 
     def move_bottom(self, agent):
         self.environment.robot_is_moving()
-        self.save_start_pos(agent)
         self.get_logger().warn(f"move backwards command send")
-        self._send_drive_goal(agent, -0.3, 0.0)
+        self.rotate_robot(agent, 0.3, 3.14)
         self.get_logger().warn(f"function move_bottom finished")
 
     # ------------------------------------
